74e93673983c7b6cf97c36d66daec4c5486c2555
ranesi <asdkjalsdkjaslkd@saldkjasld.net>
2017-04-30 19:48:31 -0500

    fixed import error

--- ta_web/models.py
+++ ta_web/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-import .calc
-import .eq
+from .calc import *
+from .eq import *
 
-User._meta.get_field('email')._blank = False # TODO email validator
+User._meta.get_field('email')._blank = False  # TODO email validator
+
 
 class Profile(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
     signup_date = models.DateTimeField()
     description = models.CharField(max_length=200, null=True)
-    website_url = models.CharField(max_length=200, null=True) # TODO url validator
+    website_url = models.CharField(
+        max_length=200, null=True)  # TODO url validator
 
     def __str__(self):
         return '{}, {}'.format(self.user.username, self.signup_date)
+
 
 class Document(models.Model):
     title = models.CharField(max_length=200)
@@ -38,13 +42,15 @@ class Document(models.Model):
         self.date_submitted = timezone.now()
 
         self.sentences, self.words, self.syllables, \
-        self.characters, self.poly_syllables = calc.analyze_string(self.text)
+            self.characters, self.poly_syllables = calc.analyze_string(
+                self.text)
 
-        self.readability_index = eq.fk_re(self.words, self.sentences, self.syllables)
-        self.fk_grade_level = eq.fk_gl(self.words, self.sentences, self.syllables)
+        self.readability_index = eq.fk_re(
+            self.words, self.sentences, self.syllables)
+        self.fk_grade_level = eq.fk_gl(
+            self.words, self.sentences, self.syllables)
         self.ari = eq.ari(self.words, self.sentences, self.characters)
         self.smog = eq.smog(self.poly_syllables)
-
 
     def __str__(self):
         return '{}, {}'.format(self.title, self.date_submitted)
--- ta_web/signals.py
+++ ta_web/signals.py
@@ -7,10 +7,12 @@ import django.core.exceptions
 # It should be noted that this information (user account extension)
 # was gleaned from <simpleisbetterthancomplex.com>
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
